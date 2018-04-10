962bcf37f9d28431ef1ced08bb6aca50117e6e75
brandongossen <brandongossen@yahoo.com>
2018-04-09 20:42:08 -0500

    -Start using components in React
    -Learn about React and create code to display how it displays html content.
    
    -Create three empty files that represent different components (WIP)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. Component is a function or object that returns some HTML.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//render - place this components html onto the page
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    // es6 => is identical to function keyword
+    return <div>Hi!</div>
+};
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+//When writing a component out just the variable (word) will be the class of a component (A type that produces instances)
+
+//To make an instance just have to wrap it in jsx tags like so <app />
+
+// ReactDOM.render(App);
+//passing a component class, but need to pass a component instance
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+// passing a component instance to ReactDOM.render; the second argument is a reference to an existing DOM node on the page. When you render this component (<App />) I want you to render it, produce some html, and then insert that html into this element that exists in our html document.
