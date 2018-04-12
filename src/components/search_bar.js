741ee46bd8ae430f502fd0963ab78ce7c3122135
brandongossen <brandongossen@yahoo.com>
2018-04-12 11:56:25 -0500

    Manipulate State
    -Manipulate the state of the SearchBar component to re-render and push all updated information from the render method to the DOM.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
 //same thing as doing: const Component = React.Component;
 
-//Event Handler Using es6
-
 class SearchBar extends Component {
     constructor(props) {
         super(props);
         //super is calling the parent method (Component)
         this.state = { term: ''};
-        //whenever we use state we initialize it by creating a new object and assigning it to this.state - the object we pass will also contain properties that we want to record on the state - in the case above we want to record the property 'term' on state, so whenever the user updates the search input term is the property that we want to record (update) that change on.
+        //whenever we use state we initialize it by creating a new object and assigning it to this.state
     }
  render() {
-     return <input onChange={(event) => console.log(event.target.value)} />;
+     return (
+         <div>
+     <input onChange={(event) => this.setState({term: event.target.value})} />
+             Value of the input: {this.state.term}
+         </div>
+     );
+     //use this.state.term here because we are nor modifying or manipulating the value of term, but we are just referencing it, like saying hey here is what the value is, so it's okay to reference it like this, but don't do anything here like {this.state.term = '5'} where your modifying it (that is what this.setState is for!!!
+
+     //We always manipulate our state with this.setState, it informs React the state is changing and this is what the new state is.
+
+     // Only in the constructor we have this.state = an object, but to change our state everywhere else inside our component we use this.setState
  }
 }
 
 export default SearchBar;
-
- //Even Handler without using es6
-
- // class SearchBar extends Component {
- //     render() {
- //         return <input onChange={this.onInputChange}/>;
- //     }
- //
- //    onInputChange(event) {
- //        console.log(event.target.value);
- //        console.log(event)
- //    }
-
-    //the event handler here is defined as a method on the class. (The one without es6.
 
 
 
