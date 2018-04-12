f2359890ffeb9b6d8b86d58f16ceebfa9e1a147a
brandongossen <brandongossen@yahoo.com>
2018-04-11 23:07:18 -0500

    Create State
    -Initialize a state inside the SearchBar component's constructor.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,6 +4,12 @@ import React, { Component } from 'react';
 //Event Handler Using es6
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        //super is calling the parent method (Component)
+        this.state = { term: ''};
+        //whenever we use state we initialize it by creating a new object and assigning it to this.state - the object we pass will also contain properties that we want to record on the state - in the case above we want to record the property 'term' on state, so whenever the user updates the search input term is the property that we want to record (update) that change on.
+    }
  render() {
      return <input onChange={(event) => console.log(event.target.value)} />;
  }
