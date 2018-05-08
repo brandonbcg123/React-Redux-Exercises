49d1141c8eda8889e64d68030ed0d6ad9e4b5f44
brandongossen <brandongossen@yahoo.com>
2018-05-08 04:06:53 -0500

    Iterate Through Videos
    -In video_list_item.js insert a function that returns a single video item.
    
    -In video_list.js import video_list_item and create a function that gets called and iterate over the array of video components and returns a VideoListItem.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,15 @@
-import React from 'React';
+import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+   const videoItems = props.videos.map((video) => {
+        return <VideoListItem video = {video} />
+        //for each element of videos we have a function that gets called with a single video and return a VideoListItem and pass the video as a property named video.
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
-            <li></li>
+            {videoItems}
         </ul>
     );
 };
---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+    return <li>Video</li>;
+    //each video list item which is intended to show a single video, for right now will just show a single <li> that says video.
+};
+
+export default VideoListItem;
