bd29433d728043bf65531a013abbfad7dce4a99b
brandongossen <brandongossen@yahoo.com>
2018-05-09 23:33:47 -0500

    List Item Keys
    -provide each movie with a unique identifier (key) by implementing the etag of the movies as a a property.
    
    -etag is a unique identifier that can be used as a key for each video that would be give a unique key for each element in the Videoitems array. This helps React identify which id (video) to change.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,8 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
    const videoItems = props.videos.map((video) => {
-        return <VideoListItem video = {video} />
+        return <VideoListItem key={video.etag} video = {video} />
+       //etag is a unique identifier that can be used as a key for each video that would be give a unique key for each element in the Videoitems array. This helps React identify which id (video) to change.
         //for each element of videos we have a function that gets called with a single video and return a VideoListItem and pass the video as a property named video.
     });
 
