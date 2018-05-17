cacc1e619f3641cec6b2403691db3bc8b6af5282
brandongossen <brandongossen@yahoo.com>
2018-05-17 00:49:04 -0500

    Display the list of Videos
    -In video_list_item.js get access to the videos inside of VideoListItem in video_list.js by creating a video variable that equals props.video.
    
    -Use ES6 to clean the code up by replacing the props argument to ({video})...({video}) is completely identical to const video = props.video
    
    -Use the response given back from YouTube to find out the properties inside the video object (console.log(video)) and use the snippet object to obtain the properties of the videos' thumbnail (image) and title.
    
    -Place the properties of the videos' thumbnail (image) and title into variables: const imageURL and const (videoTitle).
    
    -Replace <li>Video</> up with the markup and use bootstrap to organize the list of video with the left side displaying the videos' thumbnail (image) and the right side displaying the videos' title.
    
    -Pass the thumbnail (image) to the media-object by declaring an img src attribute that references the JavaScript variable created for it inside the JSX (Use curly braces) .
    
    -Pass the title to media-heading to references the JavaScript variable created for it inside the JSX (Use curly braces) .

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Video</li>;
-    //each video list item which is intended to show a single video, for right now will just show a single <li> that says video.
+// const VideoListItem = (props) => {
+//     const video = props.video;
+
+const VideoListItem = ({video}) => {
+    //the object in the arguments array has a property of video, grab that video and declare a new variable called video. ({video}) is completely identical to const video = props.video
+
+    console.log(video);
+
+    const imageURL = video.snippet.thumbnails.default.url;
+    const videoTitle = video.snippet.title;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageURL}/>
+                </div>
+
+                <div className="media-body">
+                    <div
+                        className="media-heading">{videoTitle}
+                    </div>
+                    {/*<div className="media-heading">{video.snippet.title}*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+        </li>
+
+    );
 };
 
 export default VideoListItem;
