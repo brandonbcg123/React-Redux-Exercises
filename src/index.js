344ebe38589819e01aeb80ef1a412b7472ebdf40
brandongossen <brandongossen@yahoo.com>
2018-05-22 18:40:31 -0500

    Video Detail Component Import and Fix.
    -add another initializer to App component's state called selectedVideo.
    
    -pass the selectedVideo into the VideoDetail to be able to pass a specific video, just not the first one.
    
    -Set an initial video in the YTsearch callback constructor (setting state), to cause the App component to re-render  with this.state.selectedVideo, so it will equal to the first video where it's can load a valid video (instead of being stuck on loading because the initial value of selectedVideo is null).

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAQqdxjykmMOgyIUD18WLGjGANO5KAocRo';
 
 
-
 // Create a new component. Component is a function or object that returns some HTML.
 
 //render - place this components html onto the page
@@ -23,21 +22,31 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+            //start off as null because there is not not a selected video to start with.
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-            this.setState({videos: data});
-            console.log(data);
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                //sets list of videos
+                selectedVideo: videos[0]
+            });
+            //sets an initial video
+            console.log(videos);
         });
     }
+
     render() {
         return (
             <div>
                 <SearchBar/>
-                <VideoDetail video={this.state.videos[0]}/>
+                <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList videos={this.state.videos}/>
             </div>
-        // Video List needs to get access to the videos listed on the app state, so for VideoList to reference app we pass the list of videos just by defining a property on the jsx tag. Passing data like this is referred to as passing props. The data that we are passing from app to VideoList videos is referred to as a prop, so this is passing prop videos to VideoList, anytime the app re-renders (like when we setState on the component) VideoList will get the new list as well.
+            // Video List needs to get access to the videos listed on the app state, so for VideoList to reference app we pass the list of videos just by defining a property on the jsx tag. Passing data like this is referred to as passing props. The data that we are passing from app to VideoList videos is referred to as a prop, so this is passing prop videos to VideoList, anytime the app re-renders (like when we setState on the component) VideoList will get the new list as well.
         );
     }
 
